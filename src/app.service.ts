ef9eaa7956e2cc73c1d3f61b273df4eec924a347
Lee Ji Seok <76767226+dev-jiseok@users.noreply.github.com>
2021-06-26 21:48:16 +0900

    #1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello NestJS!';
+  }
+
+  HiHello(): string{
+    return 'Hi NestJS';
   }
 }
